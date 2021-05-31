9e8aaf93eef258909711ed39f2d27e6a62b7385d
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-05-31 18:32:46 +0900

    Added patient card providers.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:nomoca_flutter/presentation/sign_up/sign_up_view.dart';
 import 'package:nomoca_flutter/constants/environment_variables.dart';
 import 'package:nomoca_flutter/constants/nomoca_api_properties.dart';
 import 'package:nomoca_flutter/data/api/api_client.dart';
 import 'package:nomoca_flutter/data/api/authentication_api.dart';
+import 'package:nomoca_flutter/data/api/patient_card_api.dart';
 import 'package:nomoca_flutter/data/repository/authentication_repository.dart';
-import 'package:nomoca_flutter/presentation/sign_up/sign_up_view.dart';
+import 'package:nomoca_flutter/data/repository/patient_card_repository.dart';
+import 'package:nomoca_flutter/presentation/patient_card/patient_card_view_model.dart';
 import 'package:nomoca_flutter/presentation/sign_up/sign_up_view_model.dart';
 
 final apiClientProvider = Provider(
@@ -30,6 +33,24 @@ final authenticationRepositoryProvider = Provider(
 final signupViewModelProvider = StateNotifierProvider.autoDispose(
   (ref) => SignUpViewModel(
     authenticationRepository: ref.read(authenticationRepositoryProvider),
+  ),
+);
+
+final patientCardApiProvider = Provider(
+  (ref) => PatientCardApiImpl(
+    apiClient: ref.read(apiClientProvider),
+  ),
+);
+
+final patientCardRepositoryProvider = Provider(
+  (ref) => PatientCardRepositoryImpl(
+    patientCardApi: ref.read(patientCardApiProvider),
+  ),
+);
+
+final patientCardViewModelProvider = StateNotifierProvider.autoDispose(
+  (ref) => PatientCardViewModel(
+    patientCardRepository: ref.read(patientCardRepositoryProvider),
   ),
 );
 
