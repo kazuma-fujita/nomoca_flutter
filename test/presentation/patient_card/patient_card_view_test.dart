7c13d7fb10cbc8a8e76bf01f71c4e1844a2a647b
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-06-07 19:53:47 +0900

    Added unit test of fetch two element of list.

--- test/presentation/patient_card/patient_card_view_test.dart
+++ test/presentation/patient_card/patient_card_view_test.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nomoca_flutter/data/entity/remote/patient_card_entity.dart';
@@ -7,13 +8,14 @@ import 'package:nomoca_flutter/main.dart';
 import 'package:nomoca_flutter/presentation/patient_card/patient_card_view.dart';
 
 void main() {
+  const contentsBaseUrl = 'https://contents-debug.nomoca.com';
+
   // Widget testはHTTP 通信が 400 - Bad Request になる仕様
   // Image.network() で画像の取得が失敗する為、400 errorになる仕組みを無効化
   setUpAll(() => HttpOverrides.global = null);
 
   group('Testing the patient card view.', () {
     testWidgets('Testing Array with one element.', (WidgetTester tester) async {
-      const contentsBaseUrl = 'https://contents-debug.nomoca.com';
       await tester.pumpWidget(ProviderScope(
         overrides: [
           patientCardProvider.overrideWithValue(
@@ -28,7 +30,33 @@ void main() {
         child: MaterialApp(home: PatientCardView()),
       ));
 
-      expect(find.byType(CircularProgressIndicator), findsNothing);
+      expect(find.text('花子'), findsOneWidget);
+      expect(find.byType(SvgPicture), findsNWidgets(2));
+      // expect(find.byType(CircularProgressIndicator), findsNothing);
+    });
+
+    testWidgets('Testing Array with two element.', (WidgetTester tester) async {
+      await tester.pumpWidget(ProviderScope(
+        overrides: [
+          patientCardProvider.overrideWithValue(
+            const AsyncData([
+              PatientCardEntity(
+                nickname: '太郎',
+                qrCodeImageUrl: '$contentsBaseUrl/qr/1344/ueR8q99hD7Ux4VrK.png',
+              ),
+              PatientCardEntity(
+                nickname: '花子',
+                qrCodeImageUrl: '$contentsBaseUrl/qr/1372/MbQRuYNDyPFxLPhY.png',
+              ),
+            ]),
+          ),
+        ],
+        child: MaterialApp(home: PatientCardView()),
+      ));
+
+      expect(find.text('太郎'), findsOneWidget);
+      expect(find.text('花子'), findsOneWidget);
+      expect(find.byType(SvgPicture), findsNWidgets(2));
     });
   });
 }
