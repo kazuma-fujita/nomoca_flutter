fe0031796037f5fc332c7dfe21b87c6b6dd1028f
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-07-08 14:41:47 +0900

    Added favorite patient card

--- lib/presentation/favorite_list_view.dart
+++ lib/presentation/favorite_list_view.dart
@@ -31,7 +31,7 @@ class FavoriteListView extends HookWidget with AssetImagePath {
                   padding: const EdgeInsets.all(16),
                   itemCount: entities.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return _buildRow(entities[index], context);
+                    return _buildRow(context, entities[index], index);
                   },
                 ),
               )
@@ -45,68 +45,146 @@ class FavoriteListView extends HookWidget with AssetImagePath {
     );
   }
 
-  Widget _buildRow(FavoriteEntity entity, BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-      // Widgetを押し込むpushアニメーションを付与
-      child: AnimatedPushMotion(
-        onTap: () async {
-          // 詳細画面へ遷移
-          await Navigator.pushNamed(context, RouteNames.institution,
-              arguments: entity.institutionId);
-        },
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                entity.images != null && entity.images!.isNotEmpty
-                    // 画像スライダー
-                    ? ImagesSlider(images: entity.images!, isParallax: true)
-                    // デフォルト施設画像表示
-                    : ParallaxCard(
-                        builder: (backgroundImageKey) {
-                          return SizedBox(
-                            key: backgroundImageKey,
-                            // 画像表示領域の高さ
-                            height: 240,
-                            child: Image(
-                              fit: BoxFit.cover,
-                              image:
-                                  AssetImage(getRandomInstitutionImagePath()),
+  Widget _buildRow(
+      BuildContext context, FavoriteEntity entity, int verticalIndex) {
+    return AnimatedPushMotion(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // 施設画像
+          GestureDetector(
+            onTap: () async {
+              // 詳細画面へ遷移
+              await Navigator.pushNamed(context, RouteNames.institution,
+                  arguments: entity.institutionId);
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 16, 8, 16),
+                  child: Stack(
+                    children: <Widget>[
+                      entity.images != null && entity.images!.isNotEmpty
+                          // 画像スライダー
+                          ? ImagesSlider(
+                              images: entity.images!, isParallax: true)
+                          // デフォルト施設画像表示
+                          : ParallaxCard(
+                              builder: (backgroundImageKey) {
+                                return SizedBox(
+                                  key: backgroundImageKey,
+                                  // 画像表示領域の高さ
+                                  height: 240,
+                                  child: Image(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                        getRandomInstitutionImagePath()),
+                                  ),
+                                );
+                              },
                             ),
-                          );
-                        },
+                      // お気に入りボタン位置
+                      Positioned(
+                        top: 24,
+                        right: 24,
+                        child: LikeButton(
+                          key: Key('like-${entity.institutionId}'),
+                          isLiked: true,
+                          onTap: (bool isLike) async {
+                            // update API実行
+                            return _updateFavorite(
+                                isLike, entity.institutionId, context);
+                          },
+                        ),
                       ),
-                // お気に入りボタン位置
-                Positioned(
-                  top: 24,
-                  right: 24,
-                  child: LikeButton(
-                    key: Key('like-${entity.institutionId}'),
-                    isLiked: true,
-                    onTap: (bool isLike) async {
-                      // update API実行
-                      return _updateFavorite(
-                          isLike, entity.institutionId, context);
-                    },
+                    ],
+                  ),
+                ),
+                // 施設名
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
+                  child: Text(
+                    entity.name,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Text(
-                entity.name,
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.black87,
-                  fontWeight: FontWeight.bold,
-                ),
+          ),
+          _buildHorizontalItem(context, entity, verticalIndex),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHorizontalItem(
+      BuildContext context, FavoriteEntity entity, int verticalIndex) {
+    return SizedBox(
+      height: 280,
+      child: PageView.builder(
+        controller: PageController(viewportFraction: 0.85),
+        itemBuilder: (context, horizontalIndex) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4),
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+              side: const BorderSide(
+                color: Colors.black12,
+                width: 1,
               ),
             ),
-          ],
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text('診察券'),
+                  TextField(
+                    controller: TextEditingController(text: null),
+                    decoration: const InputDecoration(
+                      hintText: '診察券番号を入力してください',
+                      labelText: '診察券番号',
+                    ),
+                    // style: const TextStyle(fontSize: 14),
+                  ),
+                  TextField(
+                    controller: TextEditingController(text: ''),
+                    decoration: const InputDecoration(
+                      hintText: '次回予約日時メモを入力してください',
+                      labelText: '次回予約日時メモ',
+                    ),
+                    // style: const TextStyle(fontSize: 14),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        '前回受付',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black45,
+                        ),
+                      ),
+                      SizedBox(width: 16),
+                      Text(
+                        '----/--/--  --:--',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black45,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
