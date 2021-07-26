465eef3d74c9bbe83ce98fe3734adf476f18c1fa
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-07-26 16:27:41 +0900

    main dart separate override.

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,10 @@ import 'package:nomoca_flutter/presentation/institution_view.dart';
 import 'package:nomoca_flutter/presentation/notification_detail_view.dart';
 import 'package:nomoca_flutter/presentation/patient_card_view.dart';
 import 'package:nomoca_flutter/presentation/sign_in_view.dart';
+import 'package:nomoca_flutter/presentation/sign_up_view.dart';
 import 'package:nomoca_flutter/presentation/upsert_user_view.dart';
 import 'package:nomoca_flutter/states/providers/authentication_provider.dart';
+import 'package:nomoca_flutter/states/providers/create_user_provider.dart';
 import 'package:nomoca_flutter/states/providers/delete_family_user_provider.dart';
 import 'package:nomoca_flutter/states/providers/get_institution_provider.dart';
 import 'package:nomoca_flutter/states/providers/patient_card_provider.dart';
@@ -39,7 +41,6 @@ import 'constants/db_table_names.dart';
 import 'data/entity/remote/patient_card_entity.dart';
 
 Future<void> main() async {
-  const contentsBaseUrl = 'https://contents-debug.nomoca.com';
   initEasyLoading();
   // Initializes Hive.
   await Hive.initFlutter();
@@ -47,92 +48,7 @@ Future<void> main() async {
   await Hive.openBox<User>(DBTableNames.users);
   runApp(
     ProviderScope(
-      overrides: [
-        sendShortMessageRepositoryProvider
-            .overrideWithValue(FakeSendShortMessageRepositoryImpl()),
-        authenticationRepositoryProvider
-            .overrideWithValue(FakeAuthenticationRepositoryImpl()),
-        patientCardProvider.overrideWithValue(
-          const AsyncData([
-            PatientCardEntity(
-              nickname: '太郎',
-              qrCodeImageUrl: '$contentsBaseUrl/qr/1344/ueR8q99hD7Ux4VrK.png',
-            ),
-            PatientCardEntity(
-              nickname: '花子',
-              qrCodeImageUrl: '$contentsBaseUrl/qr/1372/MbQRuYNDyPFxLPhY.png',
-            ),
-          ]),
-        ),
-        // familyUserListReducer.overrideWithValue(const AsyncValue.data([])),
-        // familyUserListReducer.overrideWithValue(const AsyncValue.data([
-        //   UserNicknameEntity(id: 1234, nickname: '花子'),
-        //   UserNicknameEntity(id: 1235, nickname: '次郎'),
-        // ])),
-        // Error (overrideWithProvider pattern)
-        // createFamilyUserProvider.overrideWithProvider(
-        //   (ref, param) => throw Exception('Error message.'),
-        // ),
-        // Loading (overrideWithProvider pattern)
-        // createFamilyUserProvider.overrideWithProvider(
-        //   (ref, param) => Future.delayed(const Duration(seconds: 5)),
-        // ),
-        // createFamilyUserProvider.overrideWithProvider(
-        //   (ref, param) => Future<void>.value(),
-        // ),
-        getInstitutionRepositoryProvider
-            .overrideWithValue(FakeGetInstitutionRepositoryImpl()),
-        fetchFavoriteListRepositoryProvider
-            .overrideWithValue(FakeFetchFavoriteListRepositoryImpl()),
-        getFavoritePatientCardRepositoryProvider
-            .overrideWithValue(FakeGetFavoritePatientCardRepositoryImpl()),
-        keywordSearchRepositoryProvider
-            .overrideWithValue(FakeKeywordSearchRepositoryImpl()),
-        updateFavoriteProvider.overrideWithProvider(
-          (param) => AutoDisposeProvider<AsyncValue<void>>(
-            (ref) => const AsyncValue.data(null),
-          ),
-        ),
-        // updateFavoriteProvider.overrideWithProvider(
-        //   (ref, param) => throw Exception('Error message.'),
-        // ),
-        fetchNotificationListRepositoryProvider
-            .overrideWithValue(FakeFetchNotificationListRepositoryImpl()),
-        // updateReadPostProvider
-        //     .overrideWithProvider((ref, param) => Future.value()),
-        updateReadPostProvider.overrideWithProvider(
-          (param) => AutoDisposeProvider<AsyncValue<void>>(
-            (ref) => const AsyncValue.data(null),
-          ),
-        ),
-        fetchFamilyUserListRepositoryProvider
-            .overrideWithValue(FakeFetchFamilyUserListRepositoryImpl()),
-        createFamilyUserProvider.overrideWithProvider(
-          (param) => AutoDisposeProvider<AsyncValue<UserNicknameEntity>>(
-            (ref) => const AsyncValue.data(
-              UserNicknameEntity(id: 1234, nickname: '花子'),
-            ),
-          ),
-        ),
-        // createFamilyUserProvider.overrideWithProvider((ref, param) =>
-        //     Future.value(const UserNicknameEntity(id: 1234, nickname: '花子'))),
-        updateFamilyUserProvider.overrideWithProvider(
-          (param) => AutoDisposeProvider<AsyncValue<UserNicknameEntity>>(
-            (ref) => const AsyncValue.data(
-              UserNicknameEntity(id: 1234, nickname: '次郎'),
-            ),
-          ),
-        ),
-        // updateFamilyUserProvider.overrideWithProvider((ref, param) =>
-        //     Future.value(const UserNicknameEntity(id: 1234, nickname: '次郎'))),
-        deleteFamilyUserProvider.overrideWithProvider(
-          (param) => AutoDisposeProvider<AsyncValue<void>>(
-            (ref) => const AsyncValue.data(null),
-          ),
-        ),
-        // deleteFamilyUserProvider
-        //     .overrideWithProvider((ref, param) => Future.value()),
-      ],
+      overrides: _overrides,
       child: MyApp(),
     ),
   );
@@ -149,7 +65,7 @@ class MyApp extends StatelessWidget {
       // Dark mode theme
       darkTheme: darkThemeData,
       routes: <String, WidgetBuilder>{
-        // RouteNames.signUp: (_) => SignUpView(),
+        RouteNames.signUp: (_) => SignUpView(),
         RouteNames.signIn: (_) => SignInView(),
         RouteNames.authentication: (_) => AuthenticationView(),
         RouteNames.patientCard: (_) => PatientCardView(),
@@ -162,3 +78,96 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+const _contentsBaseUrl = 'https://contents-debug.nomoca.com';
+final _overrides = [
+  sendShortMessageRepositoryProvider
+      .overrideWithValue(FakeSendShortMessageRepositoryImpl()),
+  createUserProvider.overrideWithProvider(
+    (param) => AutoDisposeProvider<AsyncValue<void>>(
+      (ref) => const AsyncValue.data(null),
+    ),
+  ),
+  authenticationRepositoryProvider
+      .overrideWithValue(FakeAuthenticationRepositoryImpl()),
+  patientCardProvider.overrideWithValue(
+    const AsyncData([
+      PatientCardEntity(
+        nickname: '太郎',
+        qrCodeImageUrl: '$_contentsBaseUrl/qr/1344/ueR8q99hD7Ux4VrK.png',
+      ),
+      PatientCardEntity(
+        nickname: '花子',
+        qrCodeImageUrl: '$_contentsBaseUrl/qr/1372/MbQRuYNDyPFxLPhY.png',
+      ),
+    ]),
+  ),
+  // familyUserListReducer.overrideWithValue(const AsyncValue.data([])),
+  // familyUserListReducer.overrideWithValue(const AsyncValue.data([
+  //   UserNicknameEntity(id: 1234, nickname: '花子'),
+  //   UserNicknameEntity(id: 1235, nickname: '次郎'),
+  // ])),
+  // Error (overrideWithProvider pattern)
+  // createFamilyUserProvider.overrideWithProvider(
+  //   (ref, param) => throw Exception('Error message.'),
+  // ),
+  // Loading (overrideWithProvider pattern)
+  // createFamilyUserProvider.overrideWithProvider(
+  //   (ref, param) => Future.delayed(const Duration(seconds: 5)),
+  // ),
+  // createFamilyUserProvider.overrideWithProvider(
+  //   (ref, param) => Future<void>.value(),
+  // ),
+  getInstitutionRepositoryProvider
+      .overrideWithValue(FakeGetInstitutionRepositoryImpl()),
+  fetchFavoriteListRepositoryProvider
+      .overrideWithValue(FakeFetchFavoriteListRepositoryImpl()),
+  getFavoritePatientCardRepositoryProvider
+      .overrideWithValue(FakeGetFavoritePatientCardRepositoryImpl()),
+  keywordSearchRepositoryProvider
+      .overrideWithValue(FakeKeywordSearchRepositoryImpl()),
+  updateFavoriteProvider.overrideWithProvider(
+    (param) => AutoDisposeProvider<AsyncValue<void>>(
+      (ref) => const AsyncValue.data(null),
+    ),
+  ),
+  // updateFavoriteProvider.overrideWithProvider(
+  //   (ref, param) => throw Exception('Error message.'),
+  // ),
+  fetchNotificationListRepositoryProvider
+      .overrideWithValue(FakeFetchNotificationListRepositoryImpl()),
+  // updateReadPostProvider
+  //     .overrideWithProvider((ref, param) => Future.value()),
+  updateReadPostProvider.overrideWithProvider(
+    (param) => AutoDisposeProvider<AsyncValue<void>>(
+      (ref) => const AsyncValue.data(null),
+    ),
+  ),
+  fetchFamilyUserListRepositoryProvider
+      .overrideWithValue(FakeFetchFamilyUserListRepositoryImpl()),
+  createFamilyUserProvider.overrideWithProvider(
+    (param) => AutoDisposeProvider<AsyncValue<UserNicknameEntity>>(
+      (ref) => const AsyncValue.data(
+        UserNicknameEntity(id: 1234, nickname: '花子'),
+      ),
+    ),
+  ),
+  // createFamilyUserProvider.overrideWithProvider((ref, param) =>
+  //     Future.value(const UserNicknameEntity(id: 1234, nickname: '花子'))),
+  updateFamilyUserProvider.overrideWithProvider(
+    (param) => AutoDisposeProvider<AsyncValue<UserNicknameEntity>>(
+      (ref) => const AsyncValue.data(
+        UserNicknameEntity(id: 1234, nickname: '次郎'),
+      ),
+    ),
+  ),
+  // updateFamilyUserProvider.overrideWithProvider((ref, param) =>
+  //     Future.value(const UserNicknameEntity(id: 1234, nickname: '次郎'))),
+  deleteFamilyUserProvider.overrideWithProvider(
+    (param) => AutoDisposeProvider<AsyncValue<void>>(
+      (ref) => const AsyncValue.data(null),
+    ),
+  ),
+  // deleteFamilyUserProvider
+  //     .overrideWithProvider((ref, param) => Future.value()),
+];
